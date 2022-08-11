9eb2010458386206e7c47f17cd91709a82a81649
_n0man <nouman.memon@icloud.com>
2022-08-11 14:15:09 +0530

    create empty gh-pages repo

--- src/gatling/scala/api/Constants.scala
+++
@@ -1,5 +0,0 @@
-package api
-
-object Constants {
-  val LOGIN_USER = "superman"
-}
--- src/gatling/scala/configurations/Feeders.scala
+++
@@ -1,8 +0,0 @@
-package configurations
-
-import io.gatling.core.Predef.{configuration, csv}
-import io.gatling.core.feeder.BatchableFeederBuilder
-
-object Feeders {
-  val patientName: BatchableFeederBuilder[String] = csv("patientName.csv").circular
-}
