e00d7c8c369185c2ff557483ea323274add9b39d
Arjun-Go <arjun.g@thoughtworks.com>
2022-08-16 11:26:34 +0530

    BAH-2105 | upload artifact

--- .github/archiveReports.sh
+++ .github/archiveReports.sh
@@ -3,8 +3,8 @@ set -e
 
 cd reports
 count=$(ls -d report* | wc -l)
-if [ "$count" -gt 20 ]
+if [ "$count" -gt 10 ]
 then
-  echo "deleting reports older than 5 occurrences $(ls -d report* | head -20)"
-  ls -d report* | head -20 | xargs rm -rf
+  echo "deleting reports older than 10 occurrences $(ls -d report* | head -10)"
+  ls -d report* | head -10 | xargs rm -rf
 fi
