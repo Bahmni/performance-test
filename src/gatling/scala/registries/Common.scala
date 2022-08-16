bd4f1e11471259790415cef0b9cc9a495ec21930
ramgtw <ramkumar.g@thoughtworks.com>
2022-08-16 14:34:41 +0530

    Work In Progress Fix - openCSV jar issue

--- src/gatling/scala/registries/Common.scala
+++ src/gatling/scala/registries/Common.scala
@@ -1,15 +1,22 @@
 package registries
 
-import api.HttpRequests.{getGlobalProperty, getLoginLocations}
+import api.HttpRequests._
 import configurations.MaximumResponseTimes
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
 
 object Common {
   def login(responseTimes: MaximumResponseTimes): ChainBuilder = exec(
-    getLoginLocations
+    getLoginLocations.check(
+      status.is(200),
+      responseTimeInMillis.lte(responseTimes.query.toMillis.toInt)
+    )
       .resources(
-        getGlobalProperty("locale.allowed.list")
+        getGlobalProperty("locale.allowed.list").check(
+          status.is(200),
+          responseTimeInMillis.lte(responseTimes.query.toMillis.toInt)
+        )
       )
   )
 }
